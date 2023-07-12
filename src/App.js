c743a476f79f4d92e364e2f1f9f90b083b011243
yashika14-12 <yashika14122001@gmail.com>
2023-07-12 20:33:38 +0530

    ecom changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+    <h1>E-Com Project</h1>
+    <button>normal button</button>
+    <Button>bootstrap button</Button>
+    </div>
+  );
+}
+
+export default App;
